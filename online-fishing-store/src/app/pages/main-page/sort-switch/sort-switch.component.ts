2ddc5076e304c22a6f8d5f019aaa34ecef9f4862
Valiantsin Kerakevich <valenteen_k@list.ru>
2022-03-15 14:31:03 +0300

    create sort-switch block

--- online-fishing-store/src/app/pages/main-page/sort-switch/sort-switch.component.ts
+++ online-fishing-store/src/app/pages/main-page/sort-switch/sort-switch.component.ts
@@ -1,15 +1,21 @@
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sort-switch',
   templateUrl: './sort-switch.component.html',
-  styleUrls: ['./sort-switch.component.scss']
+  styleUrls: ['./sort-switch.component.scss'],
 })
 export class SortSwitchComponent implements OnInit {
+  public raitingButtonTitle: string = 'ПО РЕЙТИНГУ ⇧';
+  public priceButtonTitle: string = 'ПО ЦЕНЕ ⇩';
+  public isSortByPrice: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  public toggleSorting(state: boolean): void {
+    this.isSortByPrice = state;
   }
 
+  ngOnInit(): void {}
 }
--- online-fishing-store/src/app/shared/components/button/button.component.ts
+++ online-fishing-store/src/app/shared/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() public title: string = '';
+  @Input() public isActive: boolean = false;
 
   constructor() {}
 
