548189b010c1939fb84b7fead9af7b253f2a2a7a
Valiantsin Kerakevich <valenteen_k@list.ru>
2022-03-30 10:04:24 +0300

    added styles when hovering over the product card, removed comments, added lines between style values

--- online-fishing-store/src/app/pages/main-page/products/products.component.ts
+++ online-fishing-store/src/app/pages/main-page/products/products.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-// import { Router } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
 
 @Component({
@@ -11,13 +10,10 @@ export class ProductsComponent implements OnInit {
   @Input() public products: Product[];
   @Output() public selectedProduct: EventEmitter <number> = new EventEmitter<number>();
   constructor(
-    // private pouter: Router
   ) {}
 
   public onSelectProduct(product){
     this.selectedProduct.emit(product.id);
-    // this.pouter.navigate(['/product-details'])
-
   }
 
   ngOnInit(): void {}
