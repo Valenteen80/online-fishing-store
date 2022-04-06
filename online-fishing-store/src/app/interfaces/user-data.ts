c920c1d8d336a60d78fffdd99f1b28ed301803d5
Valiantsin Kerakevich <valenteen_k@list.ru>
2022-04-06 09:01:18 +0300

    create FormGroup

---
+++ online-fishing-store/src/app/interfaces/user-data.ts
@@ -0,0 +1,7 @@
+export interface UserData {
+    firstName: string,
+    lastName: string,
+    address?: string,
+    phoneNumber: number,
+    gender?: string,
+  }
--- online-fishing-store/src/app/pages/profile-page/personal-data/personal-data.component.ts
+++ online-fishing-store/src/app/pages/profile-page/personal-data/personal-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserData } from 'src/app/interfaces/user-data';
 
 @Component({
   selector: 'app-personal-data',
@@ -9,12 +10,33 @@ import { FormGroup } from '@angular/forms';
 export class PersonalDataComponent implements OnInit {
   public avatarImgAltAttributeValue: string = 'photo';
   public profilePicture: string = '../../../../assets/img/profile_picture.png';
+  public form: FormGroup;
+  public formData: UserData = {
+    firstName: "Алесь",
+    lastName: "Захаренчук",
+    address: "",
+    phoneNumber: 375293734890,
+    gender: "муж",
+  }
+
+
   constructor() { }
 
-  public form: FormGroup
+  public submit(): void {
+    console.log(this.form)
+    this.formData = {...this.form.value}
+    console.log(this.formData)
+  }
 
   ngOnInit(): void {
-    this.form = new FormGroup({})
+    this.form = new FormGroup({
+      avatar: new FormControl(''),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      address: new FormControl(''),
+      phoneNumber: new FormControl('', Validators.required),
+      gender: new FormControl('')
+    })
   }
 
 }
